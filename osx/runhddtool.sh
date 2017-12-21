f0307cb13d50b07990b1e5d37025595818037db8
txacias <t.angelo@hotmail.com>
2017-12-21 09:17:37 -0500

    Fixing Mac auto install

---
+++ osx/runhddtool.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Applications/Utilites/dbtshddtool/
+sudo python /Applications/Utilites/dbtshddtool/dbts-auto.py
--- osx/runme-osx.sh
+++ osx/runme-osx.sh
@@ -11,3 +11,15 @@ sudo python -m pip install --upgrade pip
 sudo pip install postgres
 sudo pip install mysql-connector==2.1.4
 sudo pip install update mysql-connector
+
+sudo mkdir /Applications/Utilites/dbtshddtool/
+sudo mkdir /Applications/Utilites/dbtshddtool/lib/
+
+cd ../../src/
+sudo cp dbts-auto.py /Applications/Utilites/dbtshddtool/
+cd ../linux/
+sudo cp runhddtool.sh /Applications/Utilites/dbtshddtool/
+cd ../lib/
+sudo cp dbts.config /Applications/Utilites/dbtshddtool/lib/
+
+sudo chmod +x /Applications/Utilites/dbtshddtool/runhddtool.sh
