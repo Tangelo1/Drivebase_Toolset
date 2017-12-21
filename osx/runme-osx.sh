4364d4aa481a03e1804e982cd7e6d898586ca086
txacias <t.angelo@hotmail.com>
2017-12-21 15:33:46 -0500

    It works now. Smartmontools are fine.

--- osx/runme-osx.sh
+++ osx/runme-osx.sh
@@ -3,6 +3,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install smartmontools
+ln -s /usr/local/sbin/smartctl /usr/local/bin
 
 cd ../pySMART-master
 sudo python setup.py install
